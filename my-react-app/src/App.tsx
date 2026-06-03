215328480109d00d926774b1497dcf7349cf6ec8
Shivan Kathir <shivan.kathir@gmail.com>
2026-06-03 19:02:20 -0400

    update

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -22,23 +22,23 @@ function App() {
       if(response.data.prediction === 'benign'){
         setResult('benign')
       }
-      else if(response.data.prediction === 'malware'){
-        url = url.replace(/^https?:\/\//, '')
-        //Add www. if not already present
-        if (!/^www\./.test(url)) {
-          url = 'www.' + url
-        }
+      // else if(response.data.prediction === 'malware'){
+      //   url = url.replace(/^https?:\/\//, '')
+      //   //Add www. if not already present
+      //   if (!/^www\./.test(url)) {
+      //     url = 'www.' + url
+      //   }
         
-        try {
-           const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/check_link`, { url })
-          setResult(response.data.prediction)
-          console.log(response.data)
+      //   try {
+      //      const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/check_link`, { url })
+      //     setResult(response.data.prediction)
+      //     console.log(response.data)
           
-        } catch (error) {
-          console.error('Error checking link:', error)
-        }
+      //   } catch (error) {
+      //     console.error('Error checking link:', error)
+      //   }
 
-      }
+      // }
       else{
         setResult(response.data.prediction)
       }
